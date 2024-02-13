897e42c967333682f10ef9db4355c2a600fffb27
Emilio Van Der Linden <emilio.vanderlinden@rebin.be>
2024-02-13 16:26:29 +0100

    heroku ready

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ const runSensorWorker = require('./worker/sensorWorker');
 //Middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client/build")));
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 //Routes
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
 app.use("/auth", require("./routes/jwtAuth"));
 
 app.use("/dashboard", require("./routes/dashboard"));
